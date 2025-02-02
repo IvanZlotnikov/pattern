11f648d526fa7b4a24ececfaeb1ccbbce85eebab
IvZlo <learn.java4@mail.ru>
2025-02-02 17:29:09 +0300

    main

---
+++ src/main/java/pattern/chain3/Main.java
@@ -0,0 +1,14 @@
+package pattern.chain3;
+
+public class Main {
+    public static void main(String[] args) {
+        ValidateTransaction first = new MinLimit();
+        ValidateTransaction second = new MaxLimit();
+
+        first.setNext(second);
+
+
+//        first.check(new MoneyTransaction(5_000));
+        first.check(new MoneyTransaction(12_000));
+    }
+}
