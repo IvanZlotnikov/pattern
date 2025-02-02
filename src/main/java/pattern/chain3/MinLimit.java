a5f290254df7873e8d31e41b02f403b3d0b29692
IvZlo <learn.java4@mail.ru>
2025-02-02 17:28:30 +0300

    min

---
+++ src/main/java/pattern/chain3/MinLimit.java
@@ -0,0 +1,11 @@
+package pattern.chain3;
+
+public class MinLimit extends ValidateTransaction {
+    @Override
+    public void check(MoneyTransaction moneyTransaction) {
+        if (moneyTransaction.getAmount() > 1_000) {
+            System.out.println("Необходимо ввести пин-код");
+        }
+        checkNext(moneyTransaction);
+    }
+}
