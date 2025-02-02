0bf077957e7ec9ae6acad45bbe9c573586e010ee
IvZlo <learn.java4@mail.ru>
2025-02-02 17:28:20 +0300

    max

---
+++ src/main/java/pattern/chain3/MaxLimit.java
@@ -0,0 +1,10 @@
+package pattern.chain3;
+
+public class MaxLimit extends ValidateTransaction {
+    @Override
+    public void check(MoneyTransaction moneyTransaction) {
+        if (moneyTransaction.getAmount() > 10_000) {
+            System.out.println("Превышен максимальный размер транзакции");
+        }
+    }
+}
