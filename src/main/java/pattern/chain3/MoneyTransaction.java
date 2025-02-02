6253864cc935e2667c36b01f36da3dc4a7cfd775
IvZlo <learn.java4@mail.ru>
2025-02-02 17:29:47 +0300

    transaction money

---
+++ src/main/java/pattern/chain3/MoneyTransaction.java
@@ -0,0 +1,13 @@
+package pattern.chain3;
+
+public class MoneyTransaction {
+    private double amount;
+
+    public MoneyTransaction(double amount) {
+        this.amount = amount;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+}
